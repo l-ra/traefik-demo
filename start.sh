a23bab43bcbd1206d132cc6be952691718079d4e
lra <lra@r73.info>
2020-02-29 13:17:58 +0100

    wip

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ fi
 sudo chown root:root $DIR/certs/acme.json
 sudo chmod 600 $DIR/certs/acme.json
 
-export BASIC_AUTH_ENTRY=$(echo ${BASIC_AUTH_PASSWORD} | docker run -i --rm  httpd htpasswd -ni ${BASIC_AUTH_USER})
+export BASIC_AUTH_ENTRY=$(echo ${BASIC_AUTH_PASSWORD} | docker run -i --rm httpd htpasswd -ni ${BASIC_AUTH_USER})
 
 . ${DIR}/setenv
 docker-compose up ${DAEMON} -f ${DIR}/docker-compose.yml 
