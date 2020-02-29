6a885d1486afd9777a7fc3872560660738a85b34
lra <lra@r73.info>
2020-02-29 16:16:38 +0100

    wip

--- start.sh
+++ start.sh
@@ -12,7 +12,13 @@ fi
 sudo chown root:root $DIR/certs/acme.json
 sudo chmod 600 $DIR/certs/acme.json
 
-export BASIC_AUTH_ENTRY=$(echo ${BASIC_AUTH_PASSWORD} | docker run -i --rm httpd htpasswd -ni ${BASIC_AUTH_USER})
+BASIC_AUTH_FILE=$DIR/.traefik-basic-auth 
+if [ ! -f $BASIC_AUTH_FILE ]; then
+    echo ${BASIC_AUTH_PASSWORD} | docker run -i --rm httpd htpasswd -ni ${BASIC_AUTH_USER} > ${BASIC_AUTH_FILE}
+    chmod 600 ${BASIC_AUTH_FILE}
+fi
+
+export BASIC_AUTH_ENTRY=$(cat ${BASIC_AUTH_FILE})
 
 . ${DIR}/setenv
 docker-compose -f ${DIR}/docker-compose.yml up ${DAEMON}  
--- stop.sh
+++ stop.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-. setenv
+DIR=$(dirname $0)
+SETENV=${DIR}/setenv
+
+if [ ! -f ${SETENV} ]; then 
+  echo ${SETENV} file is missing. Use setenv.tmpl as template 
+  exit 1
+fi
+
+. ${SETENV}
+
+export BASIC_AUTH_ENTRY=$(cat ${BASIC_AUTH_FILE})
+
 docker-compose down 
